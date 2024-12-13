f69d8217a4116f371076b8a7fb10009767bb4f67
unknown <pomogisebesry@gmail.com>
2024-12-13 16:04:44 +1000

    fix readme

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import '../styles/main.scss'
 import { AppProvider } from '@/components/layout/context/AppProvider'
 import { Header } from '@/components/layout/Header/Header'
 import { Footer } from '@/components/layout/Footer/Footer'
+import { useState } from 'react'
 export const metadata: Metadata = {
 	title: 'Новогодние подарки от DNS',
 	description: 'Новогодние подарки от DNS',
